0389604515d05aa883b192408e569d0d0049bc57
JB Hutch <jbhutch01@gmail.com>
2023-02-24 14:28:52 -0500

    Preload css

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,10 +1,20 @@
-module.exports = {
+const handlePurge = (ctx) => {
+  return ctx.file.basename !== 'main.scss'
+    ? {
+        '@fullhuman/postcss-purgecss': {
+          content: ['./app/**/*.{js,ts,jsx,tsx}'],
+          defaultExtractor: (content) =>
+            content.match(/[\w-/:[\]]+(?<!:)/g) || [],
+        },
+      }
+    : {}
+}
+
+module.exports = (ctx) => ({
+  map: ctx.options.map,
   parser: 'postcss-scss',
   plugins: {
-    // '@fullhuman/postcss-purgecss': {
-    //   content: ['./app/**/*.{js,ts,jsx,tsx}'],
-    //   defaultExtractor: (content) => content.match(/[\w-/:[\]]+(?<!:)/g) || [],
-    // },
+    ...handlePurge(ctx),
     '@csstools/postcss-sass': {},
     'postcss-import': {},
     'tailwindcss/nesting': {},
@@ -14,4 +24,4 @@ module.exports = {
       preset: 'default',
     },
   },
-}
+})
