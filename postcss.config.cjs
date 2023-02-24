8c8adec3abcdb9fbedb6d325bc0e21b76ec3693d
JB Hutch <jbhutch01@gmail.com>
2023-02-24 14:30:11 -0500

    Purge correct path

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -2,7 +2,7 @@ const handlePurge = (ctx) => {
   return ctx.file.basename !== 'main.scss'
     ? {
         '@fullhuman/postcss-purgecss': {
-          content: ['./app/**/*.{js,ts,jsx,tsx}'],
+          content: ['./src/**/*.{js,ts,jsx,tsx}'],
           defaultExtractor: (content) =>
             content.match(/[\w-/:[\]]+(?<!:)/g) || [],
         },
