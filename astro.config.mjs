38e132f874cb77a3240a1cb7011853b07987ab13
JB Hutch <jbhutch01@gmail.com>
2023-11-08 10:09:31 -0500

    Use astro Image

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,25 @@ import mdx from '@astrojs/mdx'
 import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
 import pandacss from '@pandacss/astro'
+import prefetch from '@astrojs/prefetch'
+
+// import vercel from '@astrojs/vercel/static'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bufgreencode.com',
-  integrations: [mdx(), react(), sitemap(), pandacss()],
+  integrations: [mdx(), react(), sitemap(), pandacss(), prefetch()],
   vite: {
     optimizeDeps: {
       exclude: ['@napi-rs/image'],
     },
   },
   trailingSlash: 'always',
+  // output: 'server',
+  // adapter: vercel({
+  //   speedInsights: {
+  //     enabled: true,
+  //   },
+  //   imageService: true,
+  // }),
 })
