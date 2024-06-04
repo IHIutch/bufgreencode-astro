9ca320df73ba15386eb82088a8f280dc0c26499d
JB Hutch <jbhutch01@gmail.com>
2024-06-04 14:53:23 -0400

    Fix panda styles

--- panda/recipes/prose.recipe.ts
+++ panda/recipes/prose.recipe.ts
@@ -5,39 +5,40 @@ export const proseRecipe = defineRecipe({
   description:
     'A system for making sure your typography looks great when your content in uncontrolled',
   base: {
-    '&:not(:where([class~=not-prose] *))': {
+    '&': {
       'lineHeight': '7',
       'fontSize': 'md',
-      ':where(h1)': {
+  
+      '& :where(h1):not(:where([class~=not-prose],[class~=not-prose] *))': {
         fontWeight: 'bold',
         fontSize: { base: '4xl', md: '5xl' },
         mb: { base: '4', md: '6' },
       },
-      ':where(h2)': {
+      '& :where(h2):not(:where([class~=not-prose],[class~=not-prose] *))': {
         fontWeight: 'bold',
         fontSize: { base: '2xl', md: '3xl' },
         mt: { base: '12', md: '14' },
         mb: { base: '3', md: '4' },
       },
-      ':where(h3)': {
+      '& :where(h3):not(:where([class~=not-prose],[class~=not-prose] *))': {
         fontWeight: 'semibold',
         fontSize: { base: 'xl', md: '2xl' },
         mt: { base: '8', md: '10' },
         mb: { base: '2', md: '3' },
       },
-      ':where(h4)': {
+      '& :where(h4):not(:where([class~=not-prose],[class~=not-prose] *))': {
         fontWeight: 'semibold',
         fontSize: { base: 'md', md: 'lg' },
         mt: { base: '1', md: '2' },
         mb: '2',
       },
-      ':where(p)': {
+      '& :where(p):not(:where([class~=not-prose],[class~=not-prose] *))': {
         fontWeight: 'normal',
         fontSize: 'md',
         mt: '3',
         mb: '4',
       },
-      ':where(a)': {
+      '& :where(a):not(:where([class~=not-prose],[class~=not-prose] *))': {
         fontWeight: 'medium',
         transitionProperty: 'common',
         transitionDuration: 'fast',
@@ -53,14 +54,14 @@ export const proseRecipe = defineRecipe({
           boxShadow: 'outline',
         },
       },
-      ':where(hr)': {
+      '& :where(hr):not(:where([class~=not-prose],[class~=not-prose] *))': {
         my: { base: '12', md: '14' },
         borderColor: 'gray.200',
         _dark: {
           borderColor: 'gray.600',
         },
       },
-      ':where(blockquote)': {
+      '& :where(blockquote):not(:where([class~=not-prose],[class~=not-prose] *))': {
         fontStyle: 'italic',
         fontWeight: 'semibold',
         paddingStart: '4',
@@ -71,7 +72,7 @@ export const proseRecipe = defineRecipe({
           borderStartColor: 'gray.600',
         },
       },
-      ':where(pre)': {
+      '& :where(pre):not(:where([class~=not-prose],[class~=not-prose] *))': {
         p: '4',
         rounded: 'md',
         bg: 'gray.600',
@@ -80,22 +81,22 @@ export const proseRecipe = defineRecipe({
         _dark: {
           bg: 'gray.800',
         },
-        code: {
+        '& code': {
           'fontWeight': 'normal',
           '&::before, &::after': {
             content: '""',
           },
         },
       },
-      ':where(code)': {
+      '& :where(code):not(:where([class~=not-prose],[class~=not-prose] *))': {
         'fontWeight': 'semibold',
         '&::before, &::after': {
           content: '"`"',
         },
       },
-      ':where(figure)': {
+      '& :where(figure):not(:where([class~=not-prose],[class~=not-prose] *))': {
         my: '8',
-        figcaption: {
+        '& figcaption': {
           color: 'gray.400',
           mt: '3',
           _dark: {
@@ -103,18 +104,18 @@ export const proseRecipe = defineRecipe({
           },
         },
       },
-      ':where(ul)': {
+      '& :where(ul):not(:where([class~=not-prose],[class~=not-prose] *))': {
         paddingStart: '6',
         listStyleType: 'disc',
       },
-      ':where(ol)': {
+      '& :where(ol):not(:where([class~=not-prose],[class~=not-prose] *))': {
         paddingStart: '6',
       },
-      ':where(li)': {
+      '& :where(li):not(:where([class~=not-prose],[class~=not-prose] *))': {
         paddingStart: '2',
         my: '2',
       },
-      ':where(ol>li)': {
+      '& :where(ol>li):not(:where([class~=not-prose],[class~=not-prose] *))': {
         '&::marker': {
           color: 'gray.400',
           _dark: {
@@ -122,7 +123,7 @@ export const proseRecipe = defineRecipe({
           },
         },
       },
-      ':where(ul>li)': {
+      '& :where(ul>li):not(:where([class~=not-prose],[class~=not-prose] *))': {
         '&::marker': {
           color: 'gray.500',
           _dark: {
@@ -130,41 +131,45 @@ export const proseRecipe = defineRecipe({
           },
         },
       },
-      ':where(table)': {
+      '& :where(table):not(:where([class~=not-prose],[class~=not-prose] *))': {
         width: 'full',
         my: '8',
         textAlign: 'start',
-        thead: {
+        '& thead': {
           borderBottomWidth: '1px',
           borderBottomColor: 'gray.300',
           _dark: {
             borderBottomColor: 'gray.600',
           },
         },
-        th: {
+        '& th:not(:where([class~=not-prose],[class~=not-prose] *))': {
           textAlign: 'inherit',
           fontWeight: 'semibold',
-          p: { base: '2', md: '3' },
+          p: {
+            base: '2', md: '3'
+          },
         },
-        td: {
-          p: { base: '2', md: '3' },
+        '& td:not(:where([class~=not-prose],[class~=not-prose] *))': {
           verticalAlign: 'baseline',
+          p: {
+            base: '2', md: '3'
+          },
         },
-        tbody: {
-          tr: {
+        '& tbody:not(:where([class~=not-prose],[class~=not-prose] *))': {
+          '& tr': {
             'borderBottomWidth': '1px',
             'borderBottomColor': 'gray.200',
             '_dark': {
               borderBottomColor: 'gray.600',
             },
-            ':last-of-type': {
+            '& :last-of-type': {
               borderBottomWidth: '0px',
               borderBottomColor: 'transparent',
             },
           },
         },
-        tfoot: {
-          tr: {
+        '& tfoot:not(:where([class~=not-prose],[class~=not-prose] *))': {
+          '& tr': {
             borderTopWidth: '1px',
             borderTopColor: 'gray.300',
             _dark: {
@@ -173,10 +178,10 @@ export const proseRecipe = defineRecipe({
           },
         },
       },
-      ':where(h1 + *), :where(h2 + *), :where(h3 + *), :where(h4 + *), :where(hr + *)':
-        {
-          mt: '0',
-        },
-    },
+      '& :where(h1 + *):not(:where([class~=not-prose],[class~=not-prose] *)), & :where(h2 + *):not(:where([class~=not-prose],[class~=not-prose] *)), & :where(h3 + *):not(:where([class~=not-prose],[class~=not-prose] *)), & :where(h4 + *):not(:where([class~=not-prose],[class~=not-prose] *)), & :where(hr + *):not(:where([class~=not-prose],[class~=not-prose] *))':
+      {
+        mt: '0',
+      },
+    }
   },
 })
