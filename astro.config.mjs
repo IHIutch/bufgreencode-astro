64abea714598223f5b8bb8d1409f2de9ba90f3f5
JB Hutch <jbhutch01@gmail.com>
2024-06-04 14:52:41 -0400

    Update deps, fix eslint, pandacss import

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,11 @@ import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
-import pandacss from '@pandacss/astro'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bufgreencode.com',
-  integrations: [mdx(), react(), sitemap(), pandacss()],
+  integrations: [mdx(), react(), sitemap()],
   vite: {
     optimizeDeps: {
       exclude: ['@napi-rs/image'],
